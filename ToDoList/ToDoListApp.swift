1b8f7b65626aa428d6b3874b6d550829ef890c84
ryan-montoyo <ryanmontoyo2003@gmail.com>
2024-06-21 09:39:34 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ryan Montoyo on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
