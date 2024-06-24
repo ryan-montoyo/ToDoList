a98d8168bd218e11dd3781888d67b0b779c720c8
ryan-montoyo <ryanmontoyo2003@gmail.com>
2024-06-24 14:51:02 -0400

    "finished prod"

--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -8,12 +8,61 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    @StateObject var viewModel = ProfileViewViewModel()
+    
     var body: some View {
         NavigationView{
             VStack{
-                
+                if let user = viewModel.user {
+                    // avatar
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(.blue)
+                        .frame(width: 125, height: 125)
+                        .padding()
+                    
+                    //info: name, email, member since
+                    VStack (alignment:.leading){
+                        HStack{
+                            Text("Name:")
+                                .bold()
+                            Text(user.name)
+                        }
+                        .padding()
+                        HStack{
+                            Text("Email:")
+                                .bold()
+                            Text(user.email)
+                        }
+                        .padding()
+                        HStack{
+                            Text("Member Since:")
+                                .bold()
+                            Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+                        }
+                        .padding()
+                    }
+                    .padding()
+                    
+                    // sign out
+                    Button("Log Out"){
+                        viewModel.logOut()
+                    }
+                    .tint(.red)
+                    .padding()
+                    
+                    Spacer()
+                }
+                else {
+                    Text("Loading Profile...")
+                }
             }
             .navigationTitle("Profile")
+        }
+        .onAppear{
+            viewModel.fetch()
         }
     }
 }
--- ToDoList/Views/ToDoListView.swift
+++ ToDoList/Views/ToDoListView.swift
@@ -6,18 +6,50 @@
 //
 
 import SwiftUI
+import FirebaseFirestoreSwift
 
 struct ToDoListView: View {
+    @StateObject var viewModel: ToDoListViewViewModel
+    @FirestoreQuery var items: [ToDoListItem]
+    
+    
+    init(userId: String){
+        
+        // users/id/todos/entries
+        self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos/")
+        
+        self._viewModel = StateObject(wrappedValue: ToDoListViewViewModel(userId: userId))
+    }
+    
     var body: some View {
         NavigationView{
             VStack{
-                
+                List(items) { item in
+                    ToDoListItemView(item: item)
+                        .swipeActions{
+                            Button ("Delete"){
+                                viewModel.delete(id: item.id)
+                            }
+                            .tint(.red)
+
+                        }
+                }.listStyle(PlainListStyle())
             }
             .navigationTitle("To Do List")
+            .toolbar {
+                Button {
+                    viewModel.showingNewItemView = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+
+            }.sheet(isPresented: $viewModel.showingNewItemView, content: {
+                NewItemView(newItemPresented: $viewModel.showingNewItemView)
+            })
         }
     }
 }
 
 #Preview {
-    ToDoListView()
+    ToDoListView(userId: "p6ZojU1uLzVs64LAAprbWMvwdYg2")
 }
